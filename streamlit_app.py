5aa3790c3ee65cb7b1156ee36771db79a7c9b9ab
Fytpmiao <88721728+Fytpmiao@users.noreply.github.com>
2023-04-30 23:58:47 +0800

    Update streamlit_app.py
    change the fruit type to kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -22,8 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
-streamlit.text(fruityvice_response.json()) # just write the data to the screen
 
 # take JSON version of data and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
