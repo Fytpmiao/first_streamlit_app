5e621f34eaf68c01624f62044f7af0ebf90779e3
Fytpmiao <88721728+Fytpmiao@users.noreply.github.com>
2023-05-01 00:01:30 +0800

    Update streamlit_app.py
    add user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,16 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
